e1a92fc51858d975e64159e8e23654418305dadc
Jyotismoy Kalita <144556261+JyotismoyKalita@users.noreply.github.com>
2025-11-02 13:51:50 +0530

    Fixed Example File

--- example.c
+++ example.c
@@ -9,9 +9,9 @@ int main()
     dataframe *table = dframe_readcsv(filename, ';', 256);
     dframe_shape(table);
     dframe_show(table);
-    dframe_destroy(table);
     printf("Name: %s\n", get_value(table, 1, 0));
     printf("Roll NO: %ld\n", get_value_long(table, 1, 1));
     printf("Grade: %lf\n", get_value_double(table, 1, 2));
+    dframe_destroy(table);
     return 0;
 }
